33d3de1627d72e811ded940c8adc36b972754bf6
Murat Doğan <doganmurat@gmail.com>
2021-12-15 11:33:06 +0300

    fix config

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,6 @@
-const config = {
+export default {
     logLevel: process.env.LOG_LEVEL || 'debug',
     wsPort: process.env.WS_PORT || 8080,
     certPath: process.env.CERT_PATH || 'test-certificate/localhost.crt',
     keyPath: process.env.KEY_PATH || 'test-certificate/localhost.key',
 };
-
-export default config;
