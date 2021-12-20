61fe82b13809d656bc589d681f11d5b14dfe1c9c
Murat Doğan <doganmurat@gmail.com>
2021-12-20 14:06:33 +0300

    ip rate limit = 10

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export default {
     logLevel: process.env.LOG_LEVEL || 'info',
     wsPort: process.env.WS_PORT || 8080,
-    ipRateLimitPoints: process.env.IP_RATE_LIMIT_POINTS || 60,
+    ipRateLimitPoints: process.env.IP_RATE_LIMIT_POINTS || 10,
     msgRateLimitPoints: process.env.MSG_RATE_LIMIT_POINTS || 60,
 };
